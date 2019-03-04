1fb1800873e3b0b347da37eb833c0b1bb87f6a23
dcollins4096 <none@none>
2019-03-04 15:20:23 -0600

    EE and BB

---
+++ python/p49_QU2EB.py
@@ -0,0 +1,427 @@
+import os
+import sys
+import pdb
+#sys.path.append('cmbtools')
+#sys.path = ['../cmbtools_nofits']+sys.path
+frbname = 'frbs'
+#if os.path.isdir('code') :
+#    sys.path.append('code')
+
+#import cmbtools
+import cmbtools_handler as cmbtools
+#import pyfits
+import astropy.io.fits as pyfits
+from scipy.optimize import leastsq
+from pylab import *
+import pylab
+import re
+import glob
+
+def linear_chi2(par,x,y) :
+    m = par[0]
+    y0 = par[1]
+
+    xcent = (x[0]+x[-1])/2
+
+    ymodel = m*(x-xcent)+y0
+#    ymodel = m*(x)+y0
+    
+    return(y - ymodel)
+
+
+
+def slopes_powers(car,frame, n0=1,p=1, fitlmin=1e3, fitlmax=8e3, plot_format="pdf"):
+    #base = '%s/FRBs/DD%04d_Cl%s_n0-%04d_p-%d.dat'%(car.directory,frame, "%s", n0, p)
+    #output_base = '%s_DD%04d_n0-%04d_p-%d_%s.%s'%(car.outname,frame, n0, p,"%s","%s")
+    base = '%s/%s/DD%04d_Cl%s.dat'%(car.directory,frbname,frame, "%s")
+    output_base = '%s_DD%04d_%s.%s'%(car.outname,frame,"%s","%s")
+    shortname = car.name
+    shortname_n0p = "%s_n0-%04d_p-%d"%(shortname, n0, p)
+
+#fitlmin = 5e3
+#fitlmax = 3e4
+
+
+    ClE = {}
+    ClB = {}
+
+    for ax in ['x','y','z'] :
+        ell, ClE[ax], ClB[ax] = pylab.loadtxt(base % ax,usecols=[0,1,2],unpack=True)
+        
+    ellmask = (fitlmin < ell)*(ell < fitlmax)
+
+    Eslope = {}
+    Bslope = {}
+
+    Eamp = {}
+    Bamp = {}
+
+    import pdb
+    #pdb.set_trace()
+    for ax in ['x','y','z'] :
+        mb = np.array([0,0])
+        ellmask_finite = np.logical_and(ellmask, ClE[ax] > 0)
+        ellmask_finite = np.logical_and(ellmask_finite, ClB[ax] > 0)
+
+
+        if ellmask_finite.sum() == 0:
+            make_plots=False
+            print( "Warning: No positive values of harmonics in target ell range")
+            Bslope[ax]=np.nan
+            Bamp[ax]=np.nan
+            Eslope[ax]=np.nan
+            Eamp[ax]=np.nan
+            continue
+        make_plots=True
+        x =np.log10(ell[ellmask_finite])
+
+        y = np.log10(ClE[ax][ellmask_finite])
+        res = leastsq(linear_chi2, mb, args=(x,y) )
+        Eslope[ax] = res[0][0]
+        Eamp[ax] = pow(10,res[0][1])
+        
+        y = np.log10(ClB[ax][ellmask_finite])
+        res = leastsq(linear_chi2, mb, args=(x,y) )
+        Bslope[ax] = res[0][0]
+        Bamp[ax] = pow(10,res[0][1])
+
+        
+    if make_plots:
+        coldict = {'x':'r', 'y':'g', 'z':'b'}
+        #values_dict = {}
+        ##pickle_fname = 'p49_all_values.pickle'
+        ##if glob.glob(pickle_fname) != []:
+        ##    values_dict = fPickle.load(pickle_fname)
+        #    #[shortname][frame][fit range, Eslope(3) Bslope(3) Eamp(3) Bamp(3)]
+        #if not values_dict.has_key(shortname_n0p):
+        #    values_dict[shortname_n0p]={}
+        #if not values_dict[shortname_n0p].has_key(frame):
+        #    values_dict[shortname_n0p][frame]={'fit_range':[fitlmin, fitlmax]}
+        #    values_dict[shortname_n0p][frame]['Eslope'] = Eslope
+        #    values_dict[shortname_n0p][frame]['Bslope'] = Bslope
+        #    values_dict[shortname_n0p][frame]['Eamp'] = Eamp
+        #    values_dict[shortname_n0p][frame]['Bamp'] = Bamp
+        ##fPickle.dump(values_dict,pickle_fname)
+            
+
+# plots
+        lfit = pow(10,(x[0]+x[-1])/2)
+
+        fig = plt.figure()
+        axis = fig.add_subplot(111)
+        axis.plot(ell,Eamp['x']*(ell/lfit)**(-2.43), c=[0.5,0.5,0.5], label='%0.2f'%(-2.43))
+        for ax in ['x','y','z'] :
+            axis.plot(ell,ClE[ax],'-',label='E%s %0.2f'%(ax, Eslope[ax]), c=coldict[ax])
+            axis.plot(ell,ClB[ax],'--',label='B%s %0.2f'%(ax, Bslope[ax]), c=coldict[ax])
+            axis.plot(ell,Eamp[ax]*(ell/lfit)**Eslope[ax],'k:')
+            axis.plot(ell,Bamp[ax]*(ell/lfit)**Bslope[ax],'k:')
+
+            print('%s Eslope %f Bslope %f Eamp/Bamp %f' % (ax,Eslope[ax],Bslope[ax],Eamp[ax]/Bamp[ax]))
+            
+        axis.legend()
+        axis.loglog()
+        axis.set_xlabel(r'$\ell$')
+        axis.axvspan(fitlmax,pylab.gca().get_xlim()[1],facecolor='k',alpha=0.1,ls=None)
+        axis.axvspan(pylab.gca().get_xlim()[0],fitlmin,facecolor='k',alpha=0.1,ls=None)
+        fig.savefig(output_base%("EE_BB",plot_format))
+
+        fig = plt.figure()
+        axis=fig.add_subplot(111)
+        axis.plot(ell, np.zeros_like(ell)+2.0, c=[0.5]*3)
+        for ax in ['x','y','z'] :
+            axis.plot(ell,ClE[ax]/ClB[ax],'-',label=ax, c=coldict[ax])
+
+        axis.set_title('ClE/ClB')
+        axis.legend()
+        axis.loglog()
+        axis.set_xlabel(r'$\ell$')
+        fig.savefig(output_base%("Ratio",plot_format))
+
+
+
+        fig = plt.figure()
+        axis=fig.add_subplot(111)
+        ax = 'y'
+        axis.plot(ell,ClE[ax],'-',label='E%s'%ax, c=coldict[ax])
+        axis.plot(ell,ClB[ax],'--',label='B%s'%ax, c=coldict[ax])
+        axis.plot(ell,Eamp[ax]*(ell/lfit)**Eslope[ax],'k:')
+        axis.plot(ell,Bamp[ax]*(ell/lfit)**Bslope[ax],'k:')
+
+        axis.set_xlim(500,1.3e4)
+
+#legend()
+        axis.loglog()
+
+        axis.axvspan(pylab.gca().get_xlim()[0],fitlmin,facecolor='k',alpha=0.1,ls=None)
+        axis.axvspan(fitlmax,pylab.gca().get_xlim()[1],facecolor='k',alpha=0.1,ls=None)
+
+#fig.savefig('EBfit.pdf',bbox_inches='tight')
+        axis.set_xlabel(r'$\ell$')
+        fig.savefig(output_base%("EBfit",plot_format))
+
+    return {'Eslope':Eslope,'Eamp':Eamp,'Bslope':Bslope,'Bamp':Bamp}
+
+#if len(sys.argv) > 1:
+#    rootdir = sys.argv[1]
+#    frame = int(sys.argv[2])
+#    Qlist = glob.glob(rootdir+'/DD%04d_Q[xyz]*.fits'%frame)
+#    print Qlist
+#else:
+#    rootdir = './output'
+#    Qlist = glob.glob(rootdir+'/*/DD*_Q[xyz]*.fits')
+def EBfromQU(Q,U, return_quharm=False):
+
+    if not Q.flags['C_CONTIGUOUS']:
+        Q = np.ascontiguousarray(Q)
+    if not U.flags['C_CONTIGUOUS']:
+        U = np.ascontiguousarray(U)
+    N = array(shape(Q),dtype = int32)
+    xsize = 5 * pi / 180
+    size2d = array([xsize,xsize])
+    Delta = size2d/N
+
+    print("N = ",N)
+    print("Delta = ",Delta)
+
+    Deltal = cmbtools.Delta2l(Delta,N)
+
+    Qharm = cmbtools.map2harm(Q,Delta)
+    Uharm = cmbtools.map2harm(U,Delta)
+
+    Eharm, Bharm = cmbtools.QU2EB(Qharm,Uharm,Deltal)
+
+    E = cmbtools.harm2map(Eharm,Delta)
+    B = cmbtools.harm2map(Bharm,Delta)
+    if return_quharm:
+        output = {'E':E,'B':B,'Eh':Eharm,'Bh':Bharm, 'Qh':Qharm, 'Uh':Uharm, 'Deltal':Deltal,'Delta':Delta}
+    else:
+        output = E,B,Eharm,Bharm
+    return output
+def QU2EB(rootdir,frame):
+    Qlist = glob.glob(rootdir+'/DD%04d_Q[xyz]*.fits'%frame)
+    Ulist = []
+    for Qfile in Qlist:
+        mo = re.match('(.*/DD[0-9]{4}_)Q([xyz].*)(.fits)',Qfile)
+        Ufile = mo.group(1)+'U'+mo.group(2)+'.fits'
+        Ulist.append(Ufile)
+
+    QUlist = zip(Qlist,Ulist)
+
+    for Qfile, Ufile in QUlist :
+
+        Q = array(pyfits.open(Qfile)[0].data,dtype=double)
+        U = array(pyfits.open(Ufile)[0].data,dtype=double)
+
+        N = array(shape(Q),dtype = int32)
+        xsize = 5 * pi / 180
+        size2d = array([xsize,xsize])
+        Delta = size2d/N
+        Deltal = cmbtools.Delta2l(Delta,N)
+
+        E,B, Eharm, Bharm = EBfromQU(Q,U)
+
+        lmax = Deltal[0]*N[0]
+        lbins = linspace(0,lmax,100)
+        lcent = lbins[:-1] + diff(lbins)/2.
+        
+        ClEE = cmbtools.harm2cl(Eharm,Deltal,lbins)
+        ClBB = cmbtools.harm2cl(Bharm,Deltal,lbins)
+
+        # write the output near the input
+        mo = re.match('(.*/DD[0-9]{4}_)Q([xyz].*)(.fits)',Qfile)
+        outroot = mo.group(1)
+        outsuf = mo.group(2)
+
+        Efile = outroot+'E'+outsuf+'.fits'
+        Bfile = outroot+'B'+outsuf+'.fits'
+        Clfile = outroot+'Cl'+outsuf+'.dat'
+
+        print( Efile, Bfile, Clfile)
+
+        hdu = pyfits.PrimaryHDU(E)
+        hdulist = pyfits.HDUList([hdu])
+        hdulist.writeto(Efile,overwrite=True)
+
+        hdu = pyfits.PrimaryHDU(B)
+        hdulist = pyfits.HDUList([hdu])
+        hdulist.writeto(Bfile,overwrite=True)
+
+        savetxt(Clfile, zip(lcent,ClEE,ClBB))
+
+'''
+# Plot Q/U
+figure(figsize=[20,6])
+subplot(1,4,1)
+title('Q')
+imshow(Q,interpolation='nearest')
+colorbar()
+subplot(1,4,2)
+title('U')
+imshow(U,interpolation='nearest')
+colorbar()
+
+# Plot E/B
+subplot(1,4,3)
+title('E')
+imshow(E,interpolation='nearest')
+colorbar()
+subplot(1,4,4)
+imshow(B,interpolation='nearest')
+title('B')
+colorbar()
+
+#plot Cl
+figure()
+plot(lcent,ClEE,label='EE')
+plot(lcent,ClBB,label='BB')
+loglog()
+legend()
+
+show()
+'''
+'''
+N = array([128,128],dtype = int32)
+xsize = 1 * pi / 180
+size2d = array([xsize,xsize])
+
+Delta = size2d/N
+
+print("N = ",N)
+print("Delta = ",Delta)
+
+Deltal = cmbtools.Delta2l(Delta,N)
+
+print("Deltal = ",Deltal)
+
+border = int(N[0]/10)
+mask = zeros(N)
+mask[border:-border,border:-border] = 1.0
+#mask = ones(N)
+fmask = sum(mask)/prod(N)
+
+lmax = 10000
+lbins = linspace(0,lmax,50)
+lcent = lbins[:-1] + diff(lbins)/2.
+
+# Make some E/B
+
+E = gaussian_filter(normal(size=N),2,mode='wrap')
+#B = zeros(N)
+B = normal(size=N)
+B = gaussian_filter(B,4,mode='wrap') - gaussian_filter(B,2,mode='wrap')
+
+
+#E = zeros(N)#array([[cos(3*2*pi*(i+j)/N[1]) for i in range(N[1]) ] for j in range(N[0])])
+#E = array([[cos(3*2*pi*(i+2*j)/N[1]) for i in range(N[1]) ] for j in range(N[0])])
+# + sin(2*pi*i/N[1])
+
+#B = array([[cos(3*2*pi*(i+j)/N[1]) for i in range(N[1]) ] for j in range(N[0])]);
+
+
+# Now do the analysis
+
+Eharm = cmbtools.map2harm(E,Delta)
+Bharm = cmbtools.map2harm(B,Delta)
+
+ClEE = cmbtools.harm2cl(Eharm,Deltal,lbins)
+ClBB = cmbtools.harm2cl(Bharm,Deltal,lbins)
+
+Qharm, Uharm = cmbtools.EB2QU(Eharm,Bharm,Deltal)
+
+Q = cmbtools.harm2map(Qharm,Delta) * mask
+U = cmbtools.harm2map(Uharm,Delta) * mask
+
+Qharm2 = cmbtools.map2harm(Q,Delta)
+Uharm2 = cmbtools.map2harm(U,Delta)
+
+Eharm2, Bharm2 = cmbtools.QU2EB(Qharm2,Uharm2,Deltal)
+
+#ClB = cmbtools.harm2cl(Bharm2)
+#ClE = cmbtools.harm2cl(Eharm2)
+ClEEout = cmbtools.harm2cl(Eharm2,Deltal,lbins)
+ClBBout = cmbtools.harm2cl(Bharm2,Deltal,lbins)
+
+
+E2 = cmbtools.harm2map(Eharm2,Delta)
+B2 = cmbtools.harm2map(Bharm2,Delta)
+
+
+# Plot E/B
+figure()
+subplot(1,2,1)
+title('E')
+imshow(E,interpolation='nearest')
+colorbar()
+subplot(1,2,2)
+imshow(B,interpolation='nearest')
+title('B')
+colorbar()
+
+# Plot Q/U
+figure()
+subplot(1,2,1)
+title('Q')
+imshow(Q,interpolation='nearest')
+colorbar()
+subplot(1,2,2)
+title('U')
+imshow(U,interpolation='nearest')
+colorbar()
+
+# Plot Q/U harmonics
+figure()
+subplot(2,2,1)
+imshow(real(Qharm2), interpolation='nearest')
+colorbar()
+title('Qharm2')
+subplot(2,2,2)
+title('Uharm2')
+imshow(real(Uharm2), interpolation='nearest')
+colorbar()
+subplot(2,2,3)
+imshow(imag(Qharm2), interpolation='nearest')
+colorbar()
+subplot(2,2,4)
+imshow(imag(Uharm2), interpolation='nearest')
+colorbar()
+
+
+# Plot output E/B harmonics
+figure()
+subplot(2,2,1)
+imshow(real(Eharm2), interpolation='nearest')
+colorbar()
+title('Eharm2')
+subplot(2,2,2)
+title('Bharm2')
+imshow(real(Bharm2), interpolation='nearest')
+colorbar()
+subplot(2,2,3)
+imshow(imag(Eharm2), interpolation='nearest')
+colorbar()
+subplot(2,2,4)
+imshow(imag(Bharm2), interpolation='nearest')
+colorbar()
+
+# Plot output E/B
+figure()
+subplot(1,2,1)
+title('E2')
+imshow(E2,interpolation='nearest')
+colorbar()
+subplot(1,2,2)
+title('B2')
+imshow(B2,interpolation='nearest')
+colorbar()
+
+
+# Plot input/output spectrum
+figure()
+plot(lcent,ClEE,label='EE')
+plot(lcent,ClEEout/fmask,':',lw=6,label='EEout')
+plot(lcent,ClBB,label='BB')
+plot(lcent,ClBBout/fmask,':',lw=6,label='BBout')
+legend()
+
+show()
+'''
---
+++ python/p49_fields.py
@@ -0,0 +1,209 @@
+import yt
+import numpy as np
+
+NH_Av = 1.8e21                  # [atoms/cm^2/mag] 
+N_avg = 1000 * 4.6 * 3.09e18    # [atoms/cm^2 * code_length
+verbose = False
+def get_n0(factor):
+    """    
+    C = AvCutoff * N_H/Av / N_avg
+      =  3 * 1.8e21 atoms/cm^2/mag * (1000 atoms cm^-3/code_density * 4.6pc/code_length)^{-1}
+    AvCutoff = 3 mag - the visual extinction above which there is complete depolarization
+    N_H/Av = 1.8e21 atoms/cm^2/mag - the ratio of column density of hydrogen nuclei to visual extinction
+    N_avg = (1000 cm^-3/code_density * 4.6pc/code_length) - the average collumn density of the 
+                                                            simulation in units of code_length^{-2} 
+    1 pc = 3.09e18 cm 
+    """
+    AvCutoff = 3                    # [mag]
+    NH_Av = 1.8e21                  # [atoms/cm^2/mag] 
+    N_avg = 1000 * 4.6 * 3.09e18    # [atoms/cm^2 * code_length^2)]
+
+    C = AvCutoff * NH_Av / N_avg    # C is approximately 3.8/code_length^2
+     
+    L_dense_pix = 1.                # Approximate size of a dense region in pixels (512x512 res)
+    L_dense = L_dense_pix/512.      # Approximate size of a dense region in code_length = pixels/resolution
+                                    # Dense regions ~1 pixel for 512 resolution.
+    n0 = C/L_dense * factor           # Critical density [code_density], above which there is depolarization 
+                                    # n0 is approximately 195 for L_dense_pix = 1 with factor = 1
+    n0_str = str(int(round(n0)))   
+    return n0, n0_str
+
+def add_epsilon_old(axis, factor, this_thing=yt):
+    """ 
+    makes an epsilon field for yt.
+    epsilon is a scaling factor that scales the stokes parameters 
+    for high density regions where there is depolarization. 
+    Above a visual extinction of 3 mag.
+    """
+    n0, n0_str = get_n0(factor)
+    NH_Av = 1.8e21                  # [atoms/cm^2/mag] 
+    N_avg = 1000 * 4.6 * 3.09e18    # [atoms/cm^2 * code_length^2)]
+
+    def _epsilon_local(field,data):
+        """ 
+        Calculate epsilon.
+        epsilon = code_density/(1.8e21 atoms/cm^2/mag) * 1000 atoms/cm^3/code_density * 4.6pc/code_length
+                = code_density * N_avg/NH_Av
+        [epsilon] = mag/code_length 
+        -> Extinction, Av = Integral of epsilon along line of sight.
+        """
+        p = data.get_field_parameter('p')
+        if p is None: p = 0
+        n = data['density']
+
+        epsilon = np.ones(data['density'].shape)
+        epsilon[ n <= n0 ] = (n.v)[ n <= n0 ] * N_avg/NH_Av 
+        epsilon[ n > n0 ]  = (n0**(1-p) * n.v**p)[ n > n0 ] * N_avg/NH_Av
+
+        return epsilon
+
+    this_thing.add_field('epsilon_n0-%s_%s'%(n0_str, axis), function=_epsilon_local)
+    
+    return n0_str
+
+def add_epsilon(n0,p, this_thing=yt): 
+    def _eps_local(field,data):
+        """This function calculates the Stokes Parameter "Q" along an axis x, y, or z.
+        Makes use of the depolarization factor "epsilon" using a power exponent.
+        """
+        
+        epsilon = np.ones(data['density'].shape)
+        n = data['density']
+        epsilon[ n <= n0 ] = (n.v)[ n <= n0 ]  
+        epsilon[ n > n0 ]  = (n0**(1-p) * n.v**p)[ n > n0 ]   
+  
+        return  epsilon 
+    
+    if verbose: print( 'adding yt field epsilon_n0-%04d_p-%d'%(n0,p))
+    this_thing.add_field('epsilon_n0-%04d_p-%d'%(n0,p), function=_eps_local, force_override=True)
+def add_stokes(axis, n0, p, this_thing=yt):
+    """makes a stokes field for yt.
+    axis should be x,y,z."""
+    field_horizontal = {'x':'By','y':'Bz','z':'Bx'}[axis]
+    field_vertical   = {'x':'Bz','y':'Bx','z':'By'}[axis]
+    add_epsilon(n0,p)
+    
+
+    def _Q_local(field,data):
+        """This function calculates the Stokes Parameter "Q" along an axis x, y, or z.
+        Makes use of the depolarization factor "epsilon" using a power exponent.
+        """
+        
+        n = data['density']
+        B_sq = data['Bx']**2.0 + data['By']**2.0 + data['Bz']**2.0
+
+        #epsilon = np.ones(data['density'].shape)
+        #epsilon[ n <= n0 ] = (n.v)[ n <= n0 ]  
+        #epsilon[ n > n0 ]  = (n0**(1-p) * n.v**p)[ n > n0 ]   
+        epsilon = n
+        out = ( epsilon * (((data[field_horizontal])**2.0) - ((data[field_vertical])**2.0))/B_sq )
+        out[B_sq == 0] = 0
+  
+        return out
+    
+    #Q_fname= 'Q%s_n0-%04d_p-%d'%(axis,n0,p)
+    Q_fname= 'Q%s'%(axis) #_n0-%04d_p-%d'%(axis,n0,p)
+    if verbose: print( 'adding yt field %s'%Q_fname)
+    this_thing.add_field(Q_fname, units='code_density', function=_Q_local, force_override=True)
+
+    def _U_local(field,data):
+        """Makes stokes U."""
+        
+        n = data['density']
+        B_sq = data['Bx']**2.0 + data['By']**2.0 + data['Bz']**2.0    
+
+        #epsilon = np.ones(data['density'].shape)
+        #epsilon[ n <= n0 ] = (n.v)[ n <= n0 ]  
+        #epsilon[ n > n0 ]  = n #(n0**(1-p) * n.v**p)[ n > n0 ] 
+        epsilon = n
+        
+        out = (2.0 * epsilon * ((data[field_horizontal]) * (data[field_vertical]))/B_sq)
+        out[B_sq == 0 ] = 0
+        return out 
+
+    #U_fname = 'U%s_n0-%04d_p-%d'%(axis,n0,p)
+    U_fname = 'U%s'%(axis)
+    if verbose: print( 'adding yt field %s'%U_fname)
+    this_thing.add_field(U_fname, units='g/cm**3', function=_U_local, force_override=True)
+    return Q_fname, U_fname
+
+
+def add_unweighted_stokes(axis, this_thing=yt):
+    """makes a stokes field for yt.
+    axis should be x,y,z.
+    These should test the projection and the rest of the pipeline.
+    Second it's an interesting demonstration of what Stokes traces."""
+    field_horizontal = {'x':'By','y':'Bz','z':'Bx'}[axis]
+    field_vertical   = {'x':'Bz','y':'Bx','z':'By'}[axis]
+
+    def _unweighted_Q_local(field,data):
+        """This function calculates the Stokes Parameter "Q"."""
+        B_sq = data['Bx']**2.0 + data['By']**2.0 + data['Bz']**2.0
+
+        return (data[field_horizontal]**2.0 - data[field_vertical]**2.0)/B_sq
+
+    fieldname = 'unweighted_Q%s'%axis
+    if verbose: print ("ADDING", fieldname)
+    this_thing.add_field(fieldname, units='dimensionless', function=_unweighted_Q_local)
+
+    def _unweighted_U_local(field,data):
+        """Makes stokes U."""
+        B_sq = data['Bx']**2.0 + data['By']**2.0 + data['Bz']**2.0
+
+        return 2*(data[field_horizontal]) * (data[field_vertical])/B_sq 
+
+    fieldname = 'unweighted_U%s'%axis
+    if verbose: print( "Adding", fieldname)
+    this_thing.add_field(fieldname, units='dimensionless', function=_unweighted_U_local)
+
+def add_N2(axis, n0, p, this_thing=yt):
+    """ Makes a field that when projected is a correction to the column density used
+    in calculating the polarization fraction. """
+    field_horizontal = {'x':'By','y':'Bz','z':'Bx'}[axis]
+    field_vertical   = {'x':'Bz','y':'Bx','z':'By'}[axis]
+
+    def _N2_local(field,data):
+        """ Calculate n2 = n * (cos^2(gamma)/2 - 1/3) where gamma is the 
+        inclination angle between the B field and the plane of the sky. 
+        cos(gamma) = B_sky dot B / |B_sky|*|B| 
+        e.g. Line of sight along z axis. B = (Bx,By,Bz); B_sky = (Bx,By,0)
+        cos(gamma) = Bx^2 + By^2 / (sqrt(Bx^2 + By^2) * sqrt(Bx^2 + By^2 + Bz^2))
+        cos(gamma)^2 = Bx^2 + By^2 / (Bx^2 + By^2 + Bz^2)
+
+        This function returns n2 as a dimensionless value since epsilon was calculated as dimensionless"""
+
+        B_sq = data['Bx']**2.0 + data['By']**2.0 + data['Bz']**2.0
+        cos_gamma_sq = (data[field_horizontal]**2.0 + data[field_vertical]**2.0)/B_sq
+
+        n = data['density'].in_units('code_density')
+
+        if 0:
+            n0 = 1.0
+            p=1.0
+            if hasattr(n0,'units'):
+                n0=data.ds.quan(n0,'gram/cm**3')
+            epsilon = np.ones_like(data['density'])
+            epsilon[ n <= n0 ] = (n)[ n <= n0 ]  
+            epsilon[ n > n0 ]  = (n0**(p) * n**p)[ n > n0 ]  
+        else:
+            epsilon = n #data.ds.quan(1.0,'gram/cm**3')
+        return epsilon * (0.5*cos_gamma_sq - 1/3) 
+       
+    fieldname = 'N2%s_n0-%04d_p-%d'%(axis,n0,p)
+    this_thing.add_field(fieldname, units='code_density', function=_N2_local)    
+    if verbose: print( "Added", fieldname)
+
+# Add yt fields for stokes and n2 along each axis with 
+# different cutoff density n0 and powerlaw index p
+def add_QU(this_ds):
+    for axis in ['x', 'y', 'z']:
+#   for n0 in [19,39,1945]:
+#       add_stokes(axis, n0, p=0)
+#       add_N2(axis, n0, p=0)
+        add_stokes(axis, n0=1, p=1, this_thing=this_ds)
+        add_N2(axis, n0=1, p=1, this_thing=this_ds)
+        #add_unweighted_stokes(axis)
+
+
+
+
