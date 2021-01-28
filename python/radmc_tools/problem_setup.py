9b81fb2862e3fc960089986b45af9a5d6562b2d6
dcollins4096 <dcollins4096@gmail.com>
2021-01-27 21:08:43 -0500

    clean up

--- python/radmc_tools/problem_setup.py
+++ python/radmc_tools/problem_setup.py
@@ -5,63 +5,6 @@ import numpy as np
 import math
 
 
-if 0:
-#
-# Import plotting libraries (start Python with ipython --matplotlib)
-#
-#from mpl_toolkits.mplot3d import axes3d
-#from matplotlib import pyplot as plt
-#
-# Some natural constants
-#
-    au  = 1.49598e13     # Astronomical Unit       [cm]
-    pc  = 3.08572e18     # Parsec                  [cm]
-    ms  = 1.98892e33     # Solar mass              [g]
-    ts  = 5.78e3         # Solar temperature       [K]
-    ls  = 3.8525e33      # Solar luminosity        [erg/s]
-    rs  = 6.96e10        # Solar radius            [cm]
-#
-# Monte Carlo parameters
-#
-#
-# Grid parameters
-#
-    nx       = 64
-    ny       = 64
-    nz       = 64
-    sizex    = 10*au
-    sizey    = 10*au
-    sizez    = 10*au
-#
-# Model parameters
-#
-    radius   = 5*au
-    rho0     = 1e-16
-#
-# Star parameters
-#
-    mstar    = ms
-    rstar    = rs
-    tstar    = ts
-    pstar    = np.array([0.,0.,0.])
-#
-# Make the coordinates
-#
-    xi       = np.linspace(-sizex,sizex,nx+1)
-    yi       = np.linspace(-sizey,sizey,ny+1)
-    zi       = np.linspace(-sizez,sizez,nz+1)
-    xc       = 0.5 * ( xi[0:nx] + xi[1:nx+1] )
-    yc       = 0.5 * ( yi[0:ny] + yi[1:ny+1] )
-    zc       = 0.5 * ( zi[0:nz] + zi[1:nz+1] )
-#
-# Make the dust density model
-#
-    qq       = np.meshgrid(xc,yc,zc,indexing='ij')
-    xx       = qq[0]
-    yy       = qq[1]
-    zz       = qq[2]
-    rr       = np.sqrt(xx**2+yy**2+zz**2)
-    rhod     = rho0 * np.exp(-(rr**2/radius**2)/2.0)
 #
 # Make the wavelength_micron.inp file
 #
@@ -102,32 +45,6 @@ if 1:
     orth  = np.zeros(nrang) + 1.e0
     ampl  = 0.5
     para  = ( 1.e0 - ampl*np.cos(muang*math.pi) ) / ( 1.e0 + ampl)
-if 0:
-#
-# Now the alignment vector field. This is ONLY FOR TESTING.
-#
-    alvec = np.zeros((nx,ny,nz,3))
-#alvec[:,:,:,2] = 1.0   # Vertical field config
-    rrc            = np.sqrt(xx**2+yy**2)
-    alvec[:,:,:,0] = yy/rrc  # Circular field config
-    alvec[:,:,:,1] = -xx/rrc # Circular field config
-#
-# Normalize 
-#
-    length = np.sqrt(alvec[:,:,:,0]*alvec[:,:,:,0]+alvec[:,:,:,1]*alvec[:,:,:,1]+alvec[:,:,:,2]*alvec[:,:,:,2])
-    alvec[:,:,:,0] = np.squeeze(alvec[:,:,:,0]) / ( length + 1e-60 )
-    alvec[:,:,:,1] = np.squeeze(alvec[:,:,:,1]) / ( length + 1e-60 )
-    alvec[:,:,:,2] = np.squeeze(alvec[:,:,:,2]) / ( length + 1e-60 )
-#
-# Now include the alignment efficiency. Simple model: zero near
-# midplane, then quadratically up to 1 with z, where 1 is reached
-# at a distance 'radius' from the midplane. This is ONLY FOR TESTING.
-#
-    epsal  = (zz/radius)*(zz/radius)
-    epsal[epsal>1.0] = 1.0
-    alvec[:,:,:,0]  = np.squeeze(alvec[:,:,:,0]) * epsal
-    alvec[:,:,:,1]  = np.squeeze(alvec[:,:,:,1]) * epsal
-    alvec[:,:,:,2]  = np.squeeze(alvec[:,:,:,2]) * epsal
 #
 # Write the wavelength file
 #
@@ -142,13 +59,9 @@ if 1:
 #
 if 0:
     with open('stars.inp','w+') as f:
-        f.write('2\n')
-        f.write('1 %d\n\n'%(nlam))
-        f.write('%13.6e %13.6e %13.6e %13.6e %13.6e\n\n'%(rstar,mstar,pstar[0],pstar[1],pstar[2]))
-        for value in lam:
-            f.write('%13.6e\n'%(value))
-        f.write('\n%13.6e\n'%(-tstar))
-#
+        "We don't need any stars, we'll set the temperature by hand"
+        pass
+
 # Write the grid file
 #
 if 1:
@@ -156,7 +69,7 @@ if 1:
     import numpy as np
     from yt.analysis_modules.radmc3d_export.api import RadMC3DWriter, RadMC3DSource
 
-    ds = yt.load("ca02_units_DD0490/data0490")
+    ds = yt.load("/scratch1/dcollins/Paper49d/radmc3d_pol_3/ca02_units_DD0490/data0490")
 
 
 if 1:
@@ -175,11 +88,12 @@ if 1:
     def _DumbTemp(field, data):
         return ds.arr(np.ones_like(data["density"]), 'K')*10
     ds.add_field("DumbTemp", function=_DumbTemp, units="K")
-    writer.write_dust_file("DumbTemp", "dumb_temp.inp")
+    writer.write_dust_file("DumbTemp", "dust_temperature.dat")
+
 if 1:
 
     #radmc3d complains if the alignment vector is >1, even by 1e-16.
-    #This keeps it from complaining.
+    #eps_val keeps it from complaining.
     eps_val = 1e-16
     def _bx_hat(field, data):
         eps = data.ds.quan(eps_val,'gauss')
@@ -196,8 +110,8 @@ if 1:
         output = data['magnetic_field_z']/(data['magnetic_field_strength']+eps)
         return output
     ds.add_field("bz_hat", function=_bz_hat, units="dimensionless")
-    print('write.  Kill the single 1 on the third line.')
-    writer.write_dust_file(["bx_hat","by_hat","bz_hat"], "align_mine_3.inp")
+    print('Writing the alignment file.  This is not perfect, please delete the third line from the file.')
+    writer.write_dust_file(["bx_hat","by_hat","bz_hat"], "grainalign_dir.inp")
 #
 # Dust opacity control file
 #
